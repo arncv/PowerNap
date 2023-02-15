2315949a09ef95271140a4f2f39dc245c27baccd
Arnav <arnavgoel7903@gmail.com>
2023-02-15 22:06:29 +0530

    fixed vuln

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const http = require('http');
 const fs = require('fs');
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const rateLimit = require("express-rate-limit");
+const app = express();
 const bodyParser = require('body-parser');
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP"
+});
+app.use(limiter);
+
 app.use(bodyParser.json());
 
 app.use(express.static('public'))
